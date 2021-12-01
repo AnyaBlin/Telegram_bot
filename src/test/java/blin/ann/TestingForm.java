a8d7ab2b90920f2203126f40126ee6efd02cb9d7
Ariisu <ariisutyan@gmail.com>
2021-12-01 20:15:25 +0300

    added tests

---
+++ src/test/java/blin/ann/TestingForm.java
@@ -0,0 +1,37 @@
+package blin.ann;
+
+import blin.ann.TestBase;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestingForm extends TestBase {
+
+    @Test
+    @DisplayName("Заполнение формы регистрации")
+    void fillFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Ann");
+        $("#lastName").setValue("Blini");
+        $("#userEmail").setValue("blinchiki@smetana.ru");
+        $(By.className("custom-control-label")).click();
+        $("#userNumber").setValue("88005553535");
+        $("#dateOfBirthInput").click();
+        $(By.className("react-datepicker__month-select")).selectOptionByValue("6");
+        $(By.className("react-datepicker__year-select")).selectOptionByValue("1997");
+        $$(".react-datepicker__day").findBy(Condition.text("8")).click();
+        $(By.id("subjectsInput")).setValue("Maths").pressEnter();
+        $$(By.id("hobbiesWrapper")).find(Condition.text("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/22.01.2021.png"));
+        $("#currentAddress").setValue("Some address");
+        $(By.id("react-select-3-input")).setValue("Haryana").pressEnter();
+        $(By.id("react-select-4-input")).setValue("Karnal").pressEnter();
+        $(byText("Submit")).click();
+    }
+}
---
+++ src/test/java/blin/ann/TestingFormWithPageObject.java
@@ -0,0 +1,48 @@
+package blin.ann;
+
+import blin.ann.TestBase;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+public class TestingFormWithPageObject extends TestBase {
+
+    Faker faker = new Faker(new Locale("en-IND"));
+
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String fullName = firstName + " " + lastName;
+    String userEmail = faker.internet().emailAddress();
+    String userAddress = faker.address().streetAddress();
+    String userNumber = faker.number().digits(10);
+
+    @Test
+    @DisplayName("Заполнение формы регистрации с PageObj")
+    void fillFormTest() {
+        registrationPage.openPage();
+        registrationPage.typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeEmail(userEmail)
+                .typeGender()
+                .streetAddress(userAddress)
+                .userNumber(userNumber)
+                .typeSubject()
+                .typeHobbbies("Reading")
+                .setDate("8", "6", "1997")
+                .uploadPicture()
+                .stateCity("Haryana", "Karnal")
+                .submit();
+        registrationPage.checkResults("Student Name", fullName)
+                .checkResults("Student Email", userEmail)
+                .checkResults("Gender", "Male")
+                .checkResults("Mobile", userNumber)
+                .checkResults("Date of Birth", "8 July,1997")
+                .checkResults("Subjects", "Maths")
+                .checkResults("Hobbies", "Reading")
+                .checkResults("Picture", "22.01.2021.png")
+                .checkResults("Address", userAddress)
+                .checkResults("State and City", "Haryana Karnal");
+    }
+}
